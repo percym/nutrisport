0b9463f4f71862ea48ba75564321df42a07674ff
unknown <mugadzap@gmail.com>
2026-01-21 18:16:37 +0000

    top bar showing

--- home/src/commonMain/kotlin/dev/percym/home/HomeGraphScreen.kt
+++ home/src/commonMain/kotlin/dev/percym/home/HomeGraphScreen.kt
@@ -6,13 +6,17 @@ import androidx.compose.foundation.layout.Spacer
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.height
 import androidx.compose.foundation.layout.padding
+import androidx.compose.material3.CenterAlignedTopAppBar
+import androidx.compose.material3.ExperimentalMaterial3Api
+import androidx.compose.material3.Icon
+import androidx.compose.material3.IconButton
 import androidx.compose.material3.Scaffold
+import androidx.compose.material3.Text
+import androidx.compose.material3.TopAppBar
 import androidx.compose.runtime.Composable
 import androidx.compose.runtime.derivedStateOf
 import androidx.compose.runtime.getValue
-import androidx.compose.runtime.mutableStateOf
 import androidx.compose.runtime.remember
-import androidx.compose.runtime.setValue
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.unit.dp
 import androidx.navigation.compose.NavHost
@@ -21,9 +25,16 @@ import androidx.navigation.compose.currentBackStackEntryAsState
 import androidx.navigation.compose.rememberNavController
 import dev.percym.home.common.BottomBar
 import dev.percym.home.domain.BottomBarDestination
+import dev.percym.shared.BebasNeueFont
+import dev.percym.shared.FontSize
+import dev.percym.shared.IconPrimary
 import dev.percym.shared.Surface
+import dev.percym.shared.TextPrimary
 import dev.percym.shared.navigation.Screen
+import nutrisport.shared.Resources
+import org.jetbrains.compose.resources.painterResource
 
+@OptIn(ExperimentalMaterial3Api::class)
 @Composable
 fun HomeGraphScreen(){
     val navController = rememberNavController()
@@ -42,7 +53,30 @@ fun HomeGraphScreen(){
 
 
     Scaffold(
-        containerColor= Surface
+        containerColor= Surface,
+        topBar = {
+            CenterAlignedTopAppBar(
+                title = {
+                    Text(
+                        text = selectedDestination.title,
+                        fontFamily = BebasNeueFont(),
+                        fontSize = FontSize.LARGE,
+                        color= TextPrimary
+
+                    )
+                },
+                navigationIcon = {
+                    IconButton(onClick = {}){
+                        Icon(
+                            painter = painterResource(Resources.Icon.Menu),
+                            contentDescription = "Menu icon",
+                            tint = IconPrimary
+                        )
+                    }
+                }
+            )
+        },
+
     ) {padding ->
         Column(
             modifier = Modifier.fillMaxSize().padding(top = padding.calculateTopPadding(), bottom = padding.calculateBottomPadding())
@@ -65,12 +99,12 @@ fun HomeGraphScreen(){
                     selected = selectedDestination,
                     onSelect = {destination ->
                         navController.navigate(destination.Screen){
-//                            launchSingleTop=true
-//                            popUpTo<Screen.ProductsOverview>{
-//                                saveState=true
-//                                inclusive=false
-//                            }
-//                            restoreState=true
+                            launchSingleTop=true
+                            popUpTo<Screen.ProductsOverview>{
+                                saveState=true
+                                inclusive=false
+                            }
+                            restoreState=true
                         }
 
 
